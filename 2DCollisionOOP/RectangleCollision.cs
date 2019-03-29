1d7df971a2a70e8f0f6cf005bdce9570b5533e97
Oksana Shulga <oksana.shulga27@gmail.com>
2019-03-29 12:19:14 +0200

    csproj is updated with correct structure of project

---
+++ 2DCollisionOOP/RectangleCollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DCollisionOOP
+{
+    class RectangleCollision : ICollision
+    {
+        public bool CollisionDetection(Sprite spriteA, Sprite spriteB)
+        {
+            return spriteA.Rectangle.Intersects(spriteB.Rectangle);
+        }
+    }
+}
