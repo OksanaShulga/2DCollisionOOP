4fb4d366e1cfc5cce8bc5aade7a9e4f245fb20c6
Serhii Belei <sergiy.beley@gmail.com>
2019-04-09 22:29:27 +0300

    tried to run test for RectangleCollision

--- 2DCollisionOOP.Test/Collision/RectangleCollisionTest.cs
+++ 2DCollisionOOP.Test/Collision/RectangleCollisionTest.cs
@@ -1,19 +1,27 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using NUnit.Framework;
 
 namespace _2DCollisionOOP
 {
     public class RectangleCollisionTest
     {
+        ContentManager content;
+
         [SetUp]
         public void Setup()
         {
+            //content = new ContentManager(null, "Content");
+            
         }
 
         [Test]
         public void TestCollision()
         {
             RectangleCollision collision = new RectangleCollision();
-            Assert.Pass();
+            Sprite square = new Sprite();// content.Load<Texture2D>("shield"));
+            Sprite dot = new Sprite();// content.Load<Texture2D>("dot"));
+            Assert.IsTrue( collision.CollisionDetection(square, dot) );
         }
     }
 }
